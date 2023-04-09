f35132b388868c80a373d922bac569666ac3b73c
zfgo <89813559+zfgo@users.noreply.github.com>
2023-04-08 19:29:13 -0700

    add double jumping

--- Assets/Scripts/player_controller.cs
+++ Assets/Scripts/player_controller.cs
@@ -12,6 +12,10 @@ public class player_controller : MonoBehaviour
 
     private Rigidbody rb;
     private int count;
+    private bool if_jump;
+    private int jump_count;
+    private bool in_air;
+    private int cur_jump_count;
     private float movement_x;
     private float movement_y;
 
@@ -22,6 +26,10 @@ public class player_controller : MonoBehaviour
         count = 0;
         set_count_text();
         win_text_object.SetActive(false);
+        if_jump = false;
+        in_air = false;
+        jump_count = 2;
+        cur_jump_count = jump_count;
     }
 
     void OnMove(InputValue movement_value)
@@ -41,9 +49,41 @@ public class player_controller : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if_jump |= Input.GetButtonDown("Jump");
+        if (cur_jump_count == 0)
+        {
+            cur_jump_count = jump_count;
+        }
+    }
+
+    void jump()
+    {
+        if_jump = false;
+        cur_jump_count--;
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movement_x, 0.0f, movement_y);
+
+        if (if_jump && rb.velocity.y == 0)
+        {
+            jump();
+            movement.y += 25.0f;
+            in_air = true;
+        }
+        else if (if_jump && cur_jump_count > 0 && in_air)
+        {
+            jump();
+            movement.y += 25.0f;
+            if (cur_jump_count < 1)
+            {
+                in_air = false;
+            }
+        }
+
         rb.AddForce(movement * speed);
     }
 
